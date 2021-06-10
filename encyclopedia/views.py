3a69e1de16a99a00b1dedc4c1ce438f7535a017c
cuong tran <chicuong.tran08@gmail.com>
2021-06-10 09:43:50 +0700

    create entry_page.html, anchor each entry in index.html to the coressponding entry_page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,14 @@ from . import util
 
 
 def index(request):
+    print(f'this is the type of request: {type(request)}')
+    print(f'this is the method of request: {dir(request)}')
+    print(f'this is the full request: {request.read()}')
+    print(f'this is the querydict of the request get: {print(request.META)}')
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry_page(request, title):
+    return render(request, "encyclopedia/entry_page.html", {"title":title, "content": util.get_entry(title)})
