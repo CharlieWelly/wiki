48fca712266f54277f4b20cb4eeac20dc8a7e300
cuong tran <chicuong.tran08@gmail.com>
2021-06-11 08:06:49 +0700

    move the views.search() out of views.index(), change the structure of the url from 'wiki' to '' to remove the bug. the bug here is entry_page is call whenever there is a string is input into the url, making views.search is not callable

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = "encyclopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>/", views.entry_page, name="entry_page"),
+    path("wiki/<str:title>/", views.entry_page, name="entry_page"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
-import re
+
 
 def index(request):
-    try:
-        query = request.GET['q']
-    except KeyError:
-        return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
-    else:
-        matched = []
-        q = query.lower()
-        for entry in util.list_entries():
-            if q in entry.lower() and q == entry.lower():
-                return HttpResponseRedirect(reverse("encyclopedia:entry_page", args={"title": entry, "content": util.get_entry(entry)}))
-            elif q in entry.lower():
-                matched.append(entry)
-        return render(request, "encyclopedia/search_result.html", {"matched": matched, "query": query})
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 
 def entry_page(request, title):
-    return render(request, "encyclopedia/entry_page.html", {"title":title, "content": util.get_entry(title)})
+    return render(
+        request,
+        "encyclopedia/entry_page.html",
+        {"title": title, "content": util.get_entry(title)},
+    )
 
+
+def search(request):
+    query = request.GET["q"]
+    q = query.lower()
+    matched = []
+    for entry in util.list_entries():
+        if q in entry.lower() and q == entry.lower():
+            return entry_page(request, query)
+        elif q in entry.lower():
+            matched.append(entry)
+    return render(
+        request, "encyclopedia/search_result.html", {"matched": matched, "query": query}
+    )
+
+
+def new_page(request):
+    pass
